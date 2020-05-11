d28eb4db2e5de37dffa6830d32b0445a8ef62adf
Caio Melo <caiofelipee17@gmail.com>
2020-05-10 21:58:51 -0300

    Modify something

--- src/js/components/restaurantMenu/CategoryTable.js
+++ src/js/components/restaurantMenu/CategoryTable.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Link, Collapse, IconButton} from '@material-ui/core';
 import { makeStyles} from '@material-ui/core/styles';
-
 import PauseSalesButton from './PauseSalesButton';
 import {formatMoney} from '../../utils/utils';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
